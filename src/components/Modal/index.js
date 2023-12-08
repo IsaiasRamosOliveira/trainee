e4f0d55608fd33493648899d38702f36d292abc0
Jeferson0Silva <jefersonsas12@gmail.com>
2023-12-08 15:21:32 -0300

    feat: basic function ready

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,15 +1,44 @@
 import { closeModal } from "../../utils/closeModal.js";
 import { openModal } from "../../utils/openModal.js";
 
+const container = document.querySelector("#container");
+const data = [];
+
+container.addEventListener("click", (e) => {
+  const btn = e.target.getAttribute("class");
+  const close = container.querySelector(".modal__container");
+  if (btn == "button__dots") {
+    openModal(container, ComponentModal);
+  }
+  if (btn == "close__button" || btn == "material-symbols-outlined button__img") {
+    closeModal(close);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const modal = container.querySelector(".modal__container");
+  if (modal && modal.contains && !modal.contains(e.target) && e.target.getAttribute("class") !== "button__dots") {
+    closeModal(modal);
+  }
+});
+
 function ComponentModal() {
   return `
-  <div class="modal">
-  <div class="modal__close">
-    <button class="close__button">
-      <img class="button__img" src="/src/imgs/modal/botao-fechar.png" alt="close button">
-    </button>
-  </div>
-  <div class="modal__container"></div>
-</div>
+    <div class="modal">
+      <div class="modal__container">
+        <div class="container__close">
+          <button class="close__button">
+            <span class="material-symbols-outlined button__img">
+            close
+            </span>
+          </button>
+        </div>
+        <div class="container__content"></div>
+      </div>
+    </div>
   `;
+}
+
+export default function modal() {
+  return
 }
