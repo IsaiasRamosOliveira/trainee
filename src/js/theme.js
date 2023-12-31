fb3d7a3b1467715e7e56d4bf6379e20ecce5d261
VictorAlencar00 <victordesousaa@gmail.com>
2023-12-31 16:23:57 -0300

    docs: assets paste deleted

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,6 @@
 const changeThemeButton = document.querySelector(".account__mode");
 const screenBlocks = document.querySelectorAll(
-  "#aside, #watch, #header, #tutorials"
+  "#container,#aside, #watch, #header, #tutorials"
 );
 
 function toggleThemeFunction() {
@@ -12,16 +12,16 @@ function toggleThemeFunction() {
 const dotsImgs = document.querySelectorAll(".button__dots");
 
 const imgSrcValues = [
-  "/src/assets/imgs/watch/two-dots.png",
-  "/src/assets/imgs/tutorials/two-dots.png",
-  "/src/assets/imgs/aside/musical-sign-of-two-dots.png",
+  "/src/imgs/watch/two-dots.png",
+  "/src/imgs/tutorials/two-dots.png",
+  "/src/imgs/aside/musical-sign-of-two-dots.png",
 ];
 
 function changeDotsImgs() {
   dotsImgs.forEach((img) => {
     let originalSrc = img.src;
     if (imgSrcValues.includes(img.getAttribute("src"))) {
-      img.setAttribute("src", "/src/assets/imgs/theme/twoDotsLightMode.png");
+      img.setAttribute("src", "/src/imgs/theme/twoDotsLightMode.png");
     } else {
       img.setAttribute("src", originalSrc);
     }
